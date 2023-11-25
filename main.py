db167731326eb53eb1984b6034c2a52c524f7127
August Hedman <augustlh@icloud.com>
2023-11-25 19:52:42 +0100

    :airplane:

--- main.py
+++ main.py
@@ -17,11 +17,7 @@ training_data, test_data = MNIST.give_data()
 # Lav en instants af et netværk
 model = Network()
 
-# Definer netværkets struktur
-#model.add(Input((784,)))
-#model.add(Dense(30, sigmoid))
-#model.add(Dense(10, sigmoid))
-
+# Definer netværkets struktur (784, 30, 10)
 model.add(Input((784,)))
 model.add(Dense(300, sigmoid))
 model.add(Dense(100, sigmoid))
@@ -30,16 +26,5 @@ model.add(Dense(10, sigmoid))
 # Byg netværket (initialiserer vægte og bias for lagene)
 model.build()
 
-#model.train(training_data[:10000], epochs=10, learning_rate=0.1)
-#w,b = model.compute_gradients(training_data[0])
-#model.beregn_gradienter(training_data[0])
-#model.feedForward(training_data[0][0])
-model.train2(training_data, epochs=10, test_data=test_data)
-
-#print(w, b)
-#print(ow)
-#print("---------------------------------------------------")
-#print(hw)
-
-
-#model.train(training_data, epochs=10, learning_rate=0.1,test_data=test_data[:1000])
+#Træn netværket
+model.train(training_data=training_data, epochs=10, test_data=test_data, learning_rate=0.1)
--- network.py
+++ network.py
@@ -34,60 +34,22 @@ class Network:
             a = layer.feedForward(a)
         return a
     
-    def beregn_gradienter(self, data):
-        X, y_actual = data[0], np.eye(10)[data[1]]
+    def train(self, training_data, epochs, test_data, learning_rate=0.1, generate_graphs=False):
+        # Print loss og accuracy før træning
+        total_loss = 0
+        for data in test_data:
+            total_loss += self.cost.cost(y_true=np.eye(10)[data[1]], y_pred=self.feedForward(data[0]))
 
-        self.calculate_outputs(X)
+        print("Før træning:", "Loss: ", total_loss/len(test_data), "Accuracy: ", self.evaluate(test_data))
 
-        #(30,) * (10,) * (10,)
-        l = (self.layers[-1]).activation_function(self.layers[-1].z, derivative=True) * (self.cost).cost_derivative(y_true=y_actual, y_pred=self.feedForward(X))
-        newOutputWeight = np.outer(l, self.layers[-2].a)
-        newOutputBias = l
-
-        l = np.dot(self.layers[-1].weights.T, l) * self.layers[-2].activation_function(self.layers[-2].z,derivative=True)
-        newHiddenWeight = np.outer(l, self.layers[0].a)
-        newHiddenBias = l
-        
-        #print("Output weight (shape): ", newOutputWeight.shape, "Output weight (sum): ", np.sum(newOutputWeight))
-        #print("Output bias (shape): ", newOutputBias.shape, "Output bias (sum): ", np.sum(newOutputBias))
-
-        #print("Hidden weight (shape): ", newHiddenWeight.shape, "Hidden weight (sum): ", np.sum(newHiddenWeight))
-        #print("Hidden bias (shape): ", newHiddenBias.shape, "Hidden bias (sum): ", np.sum(newHiddenBias))
-    
-       
-        (self.layers[1]).weights -= 0.1 * newHiddenWeight
-        (self.layers[1]).biases -= 0.1 * newHiddenBias
-
-        (self.layers[2]).weights -= 0.1 * newOutputWeight
-        (self.layers[2]).biases -= 0.1 * newOutputBias
-
-        #hidden_layer.weights += hidden_layer_output.T.dot(output_delta) * 0.1
-        #hidden_layer.biases += np.sum(output_delta, axis=0, keepdims=True) * 0.1
-
-        #hidden_layer.weights += X.T.dot(hidden_layer_delta) * 0.1
-        #hidden_layer.biases += np.sum(hidden_layer_delta, axis=0, keepdims=True) * 0.1
-
-        loss = self.cost.cost(y_true=y_actual, y_pred=self.feedForward(X))
-        return loss
-
-    def train(self, training_data, epochs, test_data):
-        for i in range(epochs):
-            total_loss = 0
-            for data in training_data:
-                loss = self.beregn_gradienter(data)
-                total_loss += loss
-
-            print("Epoch:", i, "Loss: ", total_loss/len(training_data), "Accuracy: ", self.evaluate(test_data))
-
-    def train2(self, training_data, epochs, test_data):
         for i in range(epochs):
             total_loss = 0
             for data in training_data:
                 nabla_w, nabla_b = self.compute_gradients(data)
 
                 for index in range(1, len(self.layers)):
-                    self.layers[index].weights -= 0.1 * nabla_w[index]
-                    self.layers[index].biases -= 0.1 * nabla_b[index]
+                    self.layers[index].weights -= learning_rate * nabla_w[index]
+                    self.layers[index].biases -= learning_rate * nabla_b[index]
 
                 loss = self.cost.cost(y_true=np.eye(10)[data[1]], y_pred=self.feedForward(data[0]))
                 total_loss += loss
@@ -101,19 +63,6 @@ class Network:
             if np.argmax(self.feedForward(data[0])) == data[1]:
                 correct += 1
         return (correct/len(test_data))
-
-
-
-
-
-
-
-
-
-
-
-
-
     
     def compute_gradients(self, data):
         # Formatering af data for et givent træningseksempel. Label onehot-encodes for at kunne udføre vektor operationer.
@@ -128,7 +77,7 @@ class Network:
         index = len(self.layers) - 1
         output_layer = self.layers[index]
         
-        # --- Beregn gradienterne for output laget ---
+        # --- Bestem nabla_C for output laget ---
         # \frac{\partial C}{\partial W^index} = \frac{\partial z^index}{\partial w^index} \cdot \frac{\partial a}{\partial z} \cdot \frac{\partial C}{\partial a}
         #                                     = a^(index - 1) \cdot y'(z^index) \cdot 2(a^index - y)
 
@@ -138,7 +87,7 @@ class Network:
         nabla_w[-1] = np.outer(error, self.layers[index-1].a)
         nabla_b[-1] = error
 
-        # --- Beregn gradienterne for de skjulte lag ---
+        # --- Bestem nalba_C for de skjulte lag ---
         for index in range(len(self.layers)-2, 0, -1):
             hidden_layer = self.layers[index]
             next_layer = self.layers[index+1]
